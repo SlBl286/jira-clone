e56fddb1c67f370a8b0db454940e049dd72e8c2d
qy286 <quy.doanduy@gmail.com>
2024-11-01 17:33:17 +0700

    init

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import { SignInCard } from '@/features/auth/components/sign-in-card'
+import React from 'react'
+
+export default function SignInPage() {
+  return (
+    <SignInCard/>
+  )
+}
